389f460b982e3fd28515b457bb8ee289530696f4
lonnen <chris.lonnen@gmail.com>
2024-09-02 12:21:09 -0700

    restructure the project
    this removes a layer of indirection and puts the whole thing in main

--- asciimonster/__main__.py
+++ asciimonster/__main__.py
@@ -1,4 +1,19 @@
-from .cli import cli
+from collections import Counter
+
+import click
+
+@click.command()
+@click.argument("input", type=click.File("rb"))
+def asciimonster(input):
+    "a cli tool for ingesting a file and vomiting out ascii character stats"
+    characters = Counter()
+    while True:
+        c = input.read(1)
+        if not c:
+            break
+        characters.update([c])
+        click.echo(c)
+
 
 if __name__ == "__main__":
-    cli()
+    asciimonster()
