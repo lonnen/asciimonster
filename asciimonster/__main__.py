d3333ca5e3c77f257ee165ca9557da8a9446f193
lonnen <chris.lonnen@gmail.com>
2024-09-03 23:07:12 -0700

    simplify
    This outputs a diy csv format. Be careful when parsing it because it does not escape the "," delimeter when it is printing out all the csv characters.
    
    This change also truncates the non-ascii characters by only outputting up to ordinal code points up to the max u8.
    
    This is a quick 'n dirty tool for assisting in the cryptopals challenge. Earlier changes started heading down the path of extra output and additional robustness but I'm not intending on publishing, promoting, or supporting this as a robust or general tool.

--- asciimonster/__main__.py
+++ asciimonster/__main__.py
@@ -2,8 +2,9 @@ from collections import Counter
 
 import click
 
+
 @click.command()
-@click.argument("input", type=click.File("rb"))
+@click.argument("input", type=click.File("r"))
 def asciimonster(input):
     "a cli tool for ingesting a file and vomiting out ascii character stats"
     characters = Counter()
@@ -12,7 +13,13 @@ def asciimonster(input):
         if not c:
             break
         characters.update([c])
-        click.echo(c)
+
+    total = float(characters.total())
+    stats = sorted(
+        [(ord(char), char, count, count / total) for char, count in characters.items()]
+    )
+    for line in [s for s in stats if s[0] < 256]:
+        click.echo(", ".join([str(item) for item in line]))
 
 
 if __name__ == "__main__":
